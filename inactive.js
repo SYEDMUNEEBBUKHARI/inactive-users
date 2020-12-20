2a92a7400574ede5046c0c51167b693b46a4f8f9
SYEDMUNEEBBUKHARI <muneebbukhari02@gmail.com>
2020-12-21 02:20:52 +0500

    express proj

--- inactive.js
+++ inactive.js
@@ -1,3 +1,8 @@
+const express=require('express')
+const app=express();
+let $Port=8000;
+let minutes=0;
+let seconds=0;
 const user = {
     name: "Qasim Asad",
     age: "25",
@@ -6,19 +11,53 @@ const user = {
 }
 
 const inactiveFirstStage = () => {
-    console.log("User has been inactive for the past 1-2 minutes");
+    console.log("User has been inactive for the past 1-2 minutes",user.lastActivity);
 }
 
 const inactiveSecondStage = () => {
-    console.log("User has been inactive for the past 3-4 minutes");
+    console.log("User has been inactive for the past 3-4 minutes",user.lastActivity);
 }
 
 const inactiveThirdStage = () => {
-    console.log("User has been inactive for the past 5-6 minutes");
+    console.log("User has been inactive for the past 5-6 minutes",user.lastActivity);
 }
 
+//action
+app.get('/action',(req,res)=>{
+    user.lastActivity= new Date().getTime();
+    minutes=0;
+    res.send("triggered route by user");
+})
 
 // this is the main method that acts like a worker and runs every 1 minute
 setInterval(function(){
     // please write your code here
+    seconds=seconds+1
+    if(seconds===60)
+    { seconds=0;
+        minutes=minutes+1;
+    }
+    if(minutes<=2)
+    {
+        inactiveFirstStage();
+    }
+    else if(minutes<=4 && minutes>2)
+    {
+        inactiveSecondStage();
+    }
+    else if(minutes<6 && minutes>4)
+    {
+        inactiveThirdStage();
+    }
+    else{
+    minutes=0;
+    }
+
+    console.log("time passing");
 }, 1000);
+
+
+
+app.listen($Port,()=>{
+    console.log("server is running on",$Port);
+})
